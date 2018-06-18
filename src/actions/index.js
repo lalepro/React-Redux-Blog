e0342e800f837a3895180b05a076583eeb04e524
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-06-18 11:57:54 -0500

    save section-9

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import {API_KEY} from './key';
+
+export const FETCH_POSTS = 'fetch_posts';
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+
+export function fetchPosts() {
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+    return {
+        type: FETCH_POSTS,
+        payload: request
+    };
+}
